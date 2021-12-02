2b082138ac4eed8e3f6521ce26b4c493e3e808ea
zyhou <richard.maxime9@gmail.com>
2021-12-02 10:33:33 +0100

    add days 2 part 1

---
+++ 02/index.ts
@@ -0,0 +1,36 @@
+type Command = {
+  action: "forward" | "down" | "up";
+  units: number;
+};
+
+const solveOne = (commands: Command[]) => {
+  let position = 0;
+  let depth = 0;
+
+  for (let index = 0; index < commands.length; index++) {
+    const command = commands[index];
+    switch (command.action) {
+      case "forward":
+        position += command.units;
+        break;
+      case "down":
+        depth += command.units;
+        break;
+      case "up":
+        depth -= command.units;
+        break;
+      default:
+        break;
+    }
+  }
+
+  return position * depth;
+};
+
+const url = new URL("input.txt", import.meta.url);
+const commands = (await Deno.readTextFile(url)).split("\n").map((line) => {
+  const [action, units] = line.split(" ");
+  return { action, units: parseInt(units, 10) } as Command;
+});
+
+console.log("Result one:", solveOne(commands));
