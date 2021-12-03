d590d9cf3b0d0252529a3a9f1c4ed541b52791a6
zyhou <richard.maxime9@gmail.com>
2021-12-03 12:21:52 +0100

    add days 3 part 2

--- 03/index.ts
+++ 03/index.ts
@@ -20,7 +20,41 @@ const solveOne = (binaries: string[]) => {
   return parseInt(gamma, 2) * parseInt(epsilon, 2);
 };
 
+const getMostCommonBit = (binaries: string[], bitIndex: number): "1" | "0" => {
+  let count0 = 0;
+  let count1 = 0;
+
+  for (const binary of binaries) {
+    if (binary[bitIndex] === "1") {
+      count1++;
+    } else {
+      count0++;
+    }
+  }
+
+  return count1 >= count0 ? "1" : "0";
+};
+
+const solveTwo = (binaries: string[]) => {
+  let oxygen = binaries;
+  let co2 = binaries;
+  const bitLength = binaries[0].length;
+
+  for (let i = 0; i < bitLength && oxygen.length > 1; i++) {
+    const mostCommonBit = getMostCommonBit(oxygen, i);
+    oxygen = oxygen.filter((line) => line[i] === mostCommonBit);
+  }
+
+  for (let i = 0; i < bitLength && co2.length > 1; i++) {
+    const mostCommonBit = getMostCommonBit(co2, i);
+    co2 = co2.filter((line) => line[i] !== mostCommonBit);
+  }
+
+  return parseInt(oxygen[0], 2) * parseInt(co2[0], 2);
+};
+
 const url = new URL("input.txt", import.meta.url);
 const binaries = (await Deno.readTextFile(url)).split("\n");
 
 console.log("Result one:", solveOne(binaries));
+console.log("Result two:", solveTwo(binaries));
