f0ba7ebb41b3651ce5db1815f7abbb10a0060470
zyhou <richard.maxime9@gmail.com>
2021-12-10 17:50:52 +0100

    add days 10 part 2

--- 10/index.ts
+++ 10/index.ts
@@ -11,6 +11,7 @@ const solveOne = (data: string[]) => {
   let count = 0;
   for (const line of data) {
     const stack = [];
+
     for (const char of [...line]) {
       if (Object.values(closeToOpen).includes(char)) {
         stack.push(char);
@@ -27,7 +28,48 @@ const solveOne = (data: string[]) => {
   return count;
 };
 
+const solveTwo = (data: string[]) => {
+  const scores: Record<string, number> = {
+    "(": 1,
+    "[": 2,
+    "{": 3,
+    "<": 4,
+  };
+
+  const closeToOpen: Record<string, string> = { ")": "(", "]": "[", "}": "{", ">": "<" };
+
+  const totals: number[] = [];
+  for (const line of data) {
+    let stack: string[] = [];
+
+    for (const char of [...line]) {
+      if (Object.values(closeToOpen).includes(char)) {
+        stack.push(char);
+        continue;
+      }
+
+      if (stack.length === 0 || stack.pop() !== closeToOpen[char]) {
+        stack = [];
+        break;
+      }
+    }
+
+    if (stack.length > 0) {
+      let subtotal = 0;
+
+      for (const char of stack.reverse()) {
+        subtotal = 5 * subtotal + scores[char];
+      }
+
+      totals.push(subtotal);
+    }
+  }
+
+  return totals.sort((a, z) => a - z)[Math.floor(totals.length / 2)];
+};
+
 const url = new URL("input.txt", import.meta.url);
 const data = (await Deno.readTextFile(url)).split("\n");
 
 console.log("Result one:", solveOne(data));
+console.log("Result two:", solveTwo(data));
